117e69ff42f9864a4909af37fb49e2651e1212a2
VladykaIhor <49124196+VladykaIhor@users.noreply.github.com>
2019-07-23 13:52:21 +0300

    Update InitServlet.java

--- academy/controller/InitServlet.java
+++ academy/controller/InitServlet.java
@@ -37,8 +37,8 @@ public class InitServlet extends HttpServlet {
         userService.add(testUser);
         userService.add(testAdmin);
         Product firstProduct = new Product("Earphones", "closed-type", 100.0);
-        Product product2 = new Product("Guitar pick", "1,00mm", 2.0);
-        Product product3 = new Product("Audio cable", "jack 3,5mm", 100.0);
+        Product secondProduct = new Product("Guitar pick", "1,00mm", 2.0);
+        Product thirdProduct = new Product("Audio cable", "jack 3,5mm", 100.0);
         productService.add(product1);
         productService.add(product2);
         productService.add(product3);
